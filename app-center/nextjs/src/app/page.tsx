f0c78e63346b9b4eff8793031b92fddefd49cb31
Nihit Reddy <nihitavr@gmail.com>
2024-04-20 13:22:23 +0530

    Removed posts from app-center

--- app-center/nextjs/src/app/page.tsx
+++ app-center/nextjs/src/app/page.tsx
@@ -1,16 +1,7 @@
-import { Suspense } from "react";
-
-import { api } from "~/trpc/server";
 import { AuthShowcase } from "./_components/auth-showcase";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
 
 export default async function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
-  const posts = api.post.all();
 
   return (
     <main className="container h-screen py-16">
@@ -19,21 +10,6 @@ export default async function HomePage() {
           Center <span className="text-primary">APP</span>
         </h1>
         <AuthShowcase />
-
-        <CreatePostForm />
-        <div className="w-full max-w-2xl overflow-y-scroll">
-          <Suspense
-            fallback={
-              <div className="flex w-full flex-col gap-4">
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-              </div>
-            }
-          >
-            <PostList posts={posts} />
-          </Suspense>
-        </div>
       </div>
     </main>
   );
