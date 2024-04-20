25e7355d463dd039c766706c7177cf52d01bf3fd
Nihit Reddy <nihitavr@gmail.com>
2024-04-20 13:47:42 +0530

    Removed posts

--- app-customer/api/src/root.ts
+++ app-customer/api/src/root.ts
@@ -2,14 +2,12 @@ import { authRouter } from "./router/auth.router";
 import { centerRouter } from "./router/center.router";
 import { cityRouter } from "./router/city.router";
 import { petRouter } from "./router/pet.router";
-import { postRouter } from "./router/post.router";
 import { serviceRouter } from "./router/service.router";
 import { userRouter } from "./router/user.router";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
-  post: postRouter,
   center: centerRouter,
   service: serviceRouter,
   city: cityRouter,
