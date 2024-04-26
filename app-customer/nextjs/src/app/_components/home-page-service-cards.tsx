f271bd1e98ceebd83217ac92693ccd376c322a21
Nihit Reddy <nihitavr@gmail.com>
2024-04-26 21:10:22 +0530

    Added homepage animation

--- app-customer/nextjs/src/app/_components/home-page-service-cards.tsx
+++ app-customer/nextjs/src/app/_components/home-page-service-cards.tsx
@@ -13,25 +13,25 @@ export default function HomePageServicesCards() {
     <div className="grid grid-cols-2 gap-3">
       <HomePageServicesCard
         name="Vet Cosultation"
-        label="Vet Clinics near you"
+        // label="Vet Clinics near you"
         imageUrl="/vet-consultation-card-image.jpg"
         link={`/${filtersStore.city.value}/centers?serviceType=veterinary`}
       />
       <HomePageServicesCard
         name="Pet Grooming"
-        label="Grooming Centers near you"
+        // label="Grooming Centers near you"
         imageUrl="/pet-grooming-card-image.jpg"
         link={`/${filtersStore.city.value}/centers?serviceType=grooming`}
       />
       <HomePageServicesCard
         name="Pet Boarding"
-        label="Boarding Centers near you"
+        // label="Boarding Centers near you"
         imageUrl="/pet-boarding-card-image.jpg"
         link={`/${filtersStore.city.value}/centers?serviceType=boarding`}
       />
       <HomePageServicesCard
         name="Home Grooming"
-        label="Home Grooming Centers near you"
+        // label="Home Grooming Centers near you"
         imageUrl="/pet-grooming-card-image.jpg"
         link={`/${filtersStore.city.value}/centers?serviceType=home_grooming`}
       />
@@ -46,7 +46,7 @@ const HomePageServicesCard = ({
   link,
 }: {
   name: string;
-  label: string;
+  label?: string;
   imageUrl: string;
   link: string;
 }) => {
@@ -60,9 +60,11 @@ const HomePageServicesCard = ({
       <span className="z-10 font-semibold text-slate-900 md:text-xl">
         {name}
       </span>
-      <span className="z-10 -mt-1 text-xs text-slate-900 opacity-60 md:text-sm">
-        {label}
-      </span>
+      {label && (
+        <span className="z-10 -mt-1 text-xs text-slate-900 opacity-60 md:text-sm">
+          {label}
+        </span>
+      )}
     </Link>
   );
 };
