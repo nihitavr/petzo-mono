faaf33723796145244156f21d4a72d316c58fb8a
Nihit Reddy <nihitavr@gmail.com>
2024-06-21 22:22:01 +0530

    Added Vercel analytics to the app - 10

--- app-customer/nextjs/src/app/checkout/services/page.tsx
+++ app-customer/nextjs/src/app/checkout/services/page.tsx
@@ -1,5 +1,25 @@
+import { auth } from "@petzo/auth-customer-app";
+import Unauthorised from "@petzo/ui/components/errors/unauthorised";
+
 import ServicesCheckoutPage from "~/app/_components/services-checkout-page";
+import SignIn from "~/app/_components/sign-in";
 
 export default async function Page() {
+  if (!(await auth())?.user) {
+    return (
+      <Unauthorised
+        comp={
+          <div className="flex flex-col items-center justify-center gap-2">
+            <span className="text-base">
+              Please <span className="font-semibold">Sign In</span> to view Your
+              Cart
+            </span>
+            <SignIn />
+          </div>
+        }
+      />
+    );
+  }
+
   return <ServicesCheckoutPage />;
 }
