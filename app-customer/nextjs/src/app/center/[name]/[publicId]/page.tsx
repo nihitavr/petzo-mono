1b6b8b27de92043bd2bde99f9512ef8512691fc7
Nihit Reddy <nihitavr@gmail.com>
2024-07-13 19:34:08 +0530

    Reformated Code - 3

--- app-customer/nextjs/src/app/center/[name]/[publicId]/page.tsx
+++ app-customer/nextjs/src/app/center/[name]/[publicId]/page.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
+import type { Metadata, ResolvingMetadata } from "next";
 
 import {
+  getCenterUrl,
   getMetadataDescription,
   getMetadataKeywords,
   getMetadataTitle,
@@ -9,11 +10,16 @@ import { api } from "~/trpc/server";
 import { RecordEvent } from "~/web-analytics/react";
 import CenterPage from "./_components/center-page";
 
-export async function generateMetadata({
-  params: { publicId },
-}: {
-  params: { publicId: string };
-}): Promise<Metadata> {
+export async function generateMetadata(
+  {
+    params: { publicId },
+  }: {
+    params: { publicId: string };
+  },
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const parentMetadata = (await parent) as Metadata;
+
   // read route params
   const center = await api.center.findByPublicId({
     publicId,
@@ -30,17 +36,21 @@ export async function generateMetadata({
   const description = getMetadataDescription(center);
   const keywords = getMetadataKeywords(center);
   const imageUrl = center.images?.[0]?.url;
+  const centerUrlPathname = getCenterUrl(center);
 
   return {
     title: title,
     description: description,
     keywords: keywords,
     openGraph: {
+      ...parentMetadata?.openGraph,
       title: title,
       description: description,
       images: imageUrl,
+      url: centerUrlPathname,
     },
     twitter: {
+      ...parentMetadata?.twitter,
       title: title,
       description: description,
       images: imageUrl,
--- app-customer/nextjs/src/app/page.tsx
+++ app-customer/nextjs/src/app/page.tsx
@@ -4,10 +4,10 @@ export default async function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
 
   return (
-    <div className="mt-16 flex flex-col gap-4 md:mt-0">
+    <div className="container-2">
       {/* <AuthShowcase /> */}
 
-      <HeroSection />
+      <HeroSection cityPublicId="bengaluru" cityName="Bengaluru" />
     </div>
   );
 }
