a4f36654fdda48293a6f5073e8da6c67c6b4dbd5
Nihit Reddy <nihitavr@gmail.com>
2024-08-01 01:58:55 +0530

    Added loading screens

---
+++ app-center/nextjs/src/app/dashboard/[centerPublicId]/services/[servicePublicId]/edit/loading.tsx
@@ -0,0 +1,5 @@
+import ProfileLoading from "~/app/dashboard/_components/profile-loading";
+
+export default function Loading() {
+  return <ProfileLoading />;
+}
---
+++ app-center/nextjs/src/app/dashboard/_components/profile-loading.tsx
@@ -0,0 +1,40 @@
+import { Skeleton } from "@petzo/ui/components/skeleton";
+
+export default function ProfileLoading() {
+  return (
+    <div className="flex flex-col gap-5">
+      <Skeleton className="h-5 w-1/2 rounded-lg bg-foreground/20" />
+      <div className="flex flex-col gap-3">
+        <Skeleton className="h-5 w-32 flex-shrink rounded-md" />
+        <Skeleton className="h-9 w-full rounded-md" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-5 w-32 flex-shrink rounded-md" />
+        <Skeleton className="h-9 w-full rounded-md" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-5 w-32 flex-shrink rounded-md" />
+        <Skeleton className="h-9 w-full rounded-md" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-5 w-32 flex-shrink rounded-md" />
+        <Skeleton className="h-9 w-full rounded-md" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-5 w-32 flex-shrink rounded-md" />
+        <Skeleton className="h-9 w-full rounded-md" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-5 w-32 flex-shrink rounded-md" />
+        <Skeleton className="h-9 w-full rounded-md" />
+      </div>
+
+      <div
+        className={`md:initial fixed bottom-0 left-0 z-50 flex w-full justify-end px-3 py-3 md:static md:px-0`}
+      >
+        {" "}
+        <Skeleton className="h-10 w-full rounded-full bg-foreground/20 md:w-32" />
+      </div>
+    </div>
+  );
+}
