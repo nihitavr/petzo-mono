0379ab1c809ab83d18afe6b70864d49c759d255f
Nihit Reddy <nihitavr@gmail.com>
2024-08-03 01:40:31 +0530

    Changed social media image for business page - 2

--- app-customer/nextjs/src/app/partner-with-us/_components/hero-section.tsx
+++ app-customer/nextjs/src/app/partner-with-us/_components/hero-section.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { TRPCClientError } from "@trpc/client";
 
+import WipeAnimation from "@petzo/ui/components/animation/wipe-animation";
 import { Button } from "@petzo/ui/components/button";
 import { Input } from "@petzo/ui/components/input";
 import { Label } from "@petzo/ui/components/label";
@@ -20,18 +21,17 @@ export default function HeroSection() {
   return (
     <div className="grid grid-cols-2 gap-44 sm:gap-0">
       <div className="col-span-2 flex flex-col pb-8 pt-2 sm:col-span-1 md:gap-1 md:pt-16">
-        <h1 className="flex flex-col text-3xl font-semibold md:text-4xl lg:text-5xl">
-          {/* <h1 className="bg-gradient-to-r from-foreground via-primary to-primary bg-clip-text text-4xl font-semibold text-transparent md:text-5xl lg:text-6xl"> */}
+        <h1 className="flex animate-fade-in flex-col gap-1 text-3xl font-semibold md:gap-2 md:text-4xl lg:text-5xl">
           <span className="inline-block">Grow Your</span>
           <span className="inline-block text-primary">Pet Care Business</span>
           <span className="inline-block">With Us</span>
         </h1>
         {/* <h1 className="text-6xl font-bold">Grow Your Business With Us</h1> */}
-        <h4 className="mt-2 text-lg font-medium text-foreground/80">
+        <h4 className="mt-2 text-lg text-foreground/80">
           In-store Grooming, Home Grooming, and Vet Consultation{" "}
         </h4>
       </div>
-      <div className="relative col-span-2 grid grid-cols-2 rounded-2xl bg-foreground/10 md:p-6 px-5 py-6">
+      <div className="relative col-span-2 grid grid-cols-2 rounded-2xl bg-foreground/10 px-5 py-6 md:p-6">
         <div className="col-span-2 flex flex-col gap-5 pb-4 pt-10 sm:col-span-1 sm:pb-8 sm:pt-8 md:gap-10">
           <span className="text-base md:text-lg">
             Your one-stop to showcase all your{" "}
@@ -87,13 +87,13 @@ export default function HeroSection() {
                   }
                 }}
               >
-                Join Us
+                Get Early Access
               </Button>
             </div>
           </div>
         </div>
 
-        <div className="absolute right-0 top-0 col-span-1 h-96 w-full px-2 sm:w-1/2 md:inline md:px-0">
+        <div className="absolute right-0 top-0 col-span-1 h-96 w-full animate-fade-in px-2 sm:w-1/2 md:inline md:px-0">
           <div className="relative h-full w-full -translate-y-[68%] md:-translate-y-[60%]">
             <Image fill src={"/partner-page/hero-image.svg"} alt="hero image" />
           </div>
