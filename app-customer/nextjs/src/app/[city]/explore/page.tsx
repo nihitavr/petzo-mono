79a71e79235ba77fba328094f297af4706103b14
Nihit Reddy <nihitavr@gmail.com>
2024-09-04 14:20:39 +0530

    Added ref event in filters page

--- app-customer/nextjs/src/app/[city]/explore/page.tsx
+++ app-customer/nextjs/src/app/[city]/explore/page.tsx
@@ -9,9 +9,9 @@ import { api } from "~/trpc/server";
 import { RecordEvent } from "~/web-analytics/react";
 
 export default async function HomePage({
-  params: { city },
+  params: { city, ref },
 }: {
-  params: { city: string };
+  params: { city: string; ref: string };
 }) {
   // You can await this here if you don't want to show Suspense fallback below
   const cities = await api.geography.getActiveCities();
@@ -19,7 +19,14 @@ export default async function HomePage({
 
   return (
     <div className="container-2 !gap-8 md:!gap-14">
-      <RecordEvent name="screenview_city_explore_home_page" data={{ city }} />
+      <RecordEvent
+        name={
+          ref
+            ? "screenview_city_explore_home_page_ref"
+            : "screenview_city_explore_home_page"
+        }
+        data={{ city, ref }}
+      />
       <HeroSection cityPublicId={city} cityName={cityName!} />
       {/* <CentersNearYouSection cityPublicId={city} /> */}
 
